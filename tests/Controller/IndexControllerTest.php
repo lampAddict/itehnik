f9dadb4562f995fff9e2bb7fcc396bca69c2384a
Roman Makarov <rk_box@mail.ru>
2018-05-07 19:44:17 +0300

    main page form test draft

--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class IndexControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexPageCreated()
     {
         $client = static::createClient();
 
@@ -16,5 +16,32 @@ class IndexControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Манифест', $crawler->filter('#manifest h5')->text());
+    }
+
+    public function testSendEmailMessageFromSite()
+    {
+        $client = static::createClient();
+
+        //get main page content
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        //main page survey
+        //get middle button on the first step
+        $fchoice = $crawler->filter('#step12')->link();
+        //click on it
+        $crawler = $client->click($fchoice);
+        //get third button on the second step
+        $schoice = $crawler->filter('#step23')->link();
+        //click on it
+        $crawler = $client->click($schoice);
+
+        //check if we get feedback form with submit button
+        $this->assertContains('Отправить', $crawler->filter('#btnSend:not(.mfp-hide)')->text());
+
+        $btn = $crawler->selectButton('btnSend');
+        
+        //click on it
+        //$crawler = $client->click($btn);
     }
 }
